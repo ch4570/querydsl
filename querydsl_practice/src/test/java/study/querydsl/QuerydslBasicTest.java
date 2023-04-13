630edee05ca8cadf05a91f2785b63c8cd7b9a408
서민재 <ch4570@naver.com>
2023-04-13 20:58:17 +0900

    [Feature] 조인 - 기본조인까지 수강 & 실습 완료

--- querydsl_practice/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl_practice/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,18 +1,22 @@
 package study.querydsl;
 
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
+import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QTeam.team;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -49,21 +53,206 @@ public class QuerydslBasicTest {
                 .setParameter("username", "member1")
                 .getSingleResult();
 
-        Assertions.assertThat(findByJQPL.getUsername()).isEqualTo("member1");
+        assertThat(findByJQPL.getUsername()).isEqualTo("member1");
     }
 
     @Test
     public void startQuerydsl() {
-        QMember m = QMember.member;
 
         // QueryDSL은 기본적으로 preparedStatement의 파라미터 바인딩 방식을 사용한다.
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
-        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").
+                        and(member.age.between(10, 30)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() {
+
+        // AND 조건일경우에는 메서드 체이닝 방식이나 여러 조건을 넣어줄 수 있다(쉼표로 구분).
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"), member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetchTest() {
+
+        // 전체 조회
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+        // 한 건 조회
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        // limit(1)을 사용한 것과 같다.
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        // 페이징에서 사용이 가능하다.
+        QueryResults<Member> result = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        result.getTotal();
+        List<Member> content = result.getResults();
+
+        // count 쿼리로 변경하는 기능을 제공한다.
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    /*
+    *   회원 정렬 순서
+    *   1. 회원 나이 내림차순(desc)
+    *   2. 회원 이름 올림차순(asc)
+    *   단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+    * */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+    @Test
+    public void paging() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(0)
+                .limit(2)
+                .fetch();
+
+        QueryResults<Member> queryResult = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(queryResult.getTotal()).isEqualTo(4);
+        assertThat(queryResult.getOffset()).isEqualTo(1);
+        assertThat(queryResult.getLimit()).isEqualTo(2);
+    }
+
+    @Test
+    public void aggregation() {
+
+        // 집계 함수 사용 예제
+        Tuple tuple = queryFactory
+                .select(member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetchOne();
+
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    /*
+    *  팀의 이름과 각 팀의 평균 연령을 구해라.
+    * */
+    @Test
+    public void groupBy() {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    /*
+    *  팀 A에 소속된 모든 회원을 찾아라.
+    * */
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /*
+    *  세타 조인 - cross join(카티시안 곱)
+    *  외부 조인이 불가능 하다 -> join on을 사용하면 외부 조인이 가능하다.
+    *  회원의 이름이 팀 이름과 같은 회원 조회
+    * */
+    @Test
+    public void thetaJoin() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
     }
 
 }
