add354694495b4c5d52c6547ffae22e054e9dfa0
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-01 22:20:43 +0900

    [Feature] 조회 API 컨트롤러 개발까지 수강 & 실습 완료

---
+++ querydsl_practice/src/main/java/study/querydsl/controller/InitMember.java
@@ -0,0 +1,44 @@
+package study.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        private EntityManager em;
+
+        @Transactional // @PostConstruct 하는 부분과 @Transactional 하는 부분은 분리해줘야한다.
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i=0; i<100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member"+i, i, selectedTeam));
+            }
+        }
+    }
+}
